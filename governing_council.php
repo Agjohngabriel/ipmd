827ce7929be377edb440ba30eb52f147064b854d
Agjohngabriel <agjohngabriel@gmail.com>
2020-02-14 08:16:58 +0100

    recent changes

--- governing_council.php
+++ governing_council.php
@@ -6,7 +6,7 @@
     
 <section id="about_intro">
     <div class="about_intro-content">
-        <h2>IPMD < Governing Council</h2>
+        <h2>IPMD > Governing Council</h2>
     </div>
 </section><!-- #intro -->
 
@@ -20,10 +20,24 @@
 								<div class="img align-self-stretch" style="background-image: url(assets/img/team-1.jpg);"></div>
 							</div>
 							<div class="text pt-3 text-center">
-								<h3>Bianca Wilson</h3>
-								<span class="position mb-2">Teacher</span>
+								<h3>Sunday A. Eke <span>(Dip. M; MCM (UK); MBIM (Lond); MIPMD, FIPMD)</span></h3>
+								<span class="position mb-2">National President</span>
 								<div class="faded">
-									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+									<p>Mr. Sunday Eke was formerly the chairman of the Mericlaims Agencies Nigeria Ltd. Before he proceedde to london for his professional education in Marketing and Management. </p>
+	              </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch">
+								<div class="img align-self-stretch" style="background-image: url(assets/img/testimonial-1.jpg);"></div>
+							</div>
+							<div class="text pt-3 text-center">
+								<h3>Professor Jacinta A. Opara <span>Bsc, Msc, MED, MBA, PHD, FIPMD, FIMC, FCIA, FBQS, FASI</span> </h3>
+								<span class="position mb-2">Chairman</span>
+								<div class="faded">
+									<p>Professor Jacinta A. Opara is a professor of international pedigress with proven track record</p>
 	              </div>
 							</div>
 						</div>
@@ -34,10 +48,24 @@
 								<div class="img align-self-stretch" style="background-image: url(assets/img/team-1.jpg);"></div>
 							</div>
 							<div class="text pt-3 text-center">
-								<h3>Mitch Parker</h3>
-								<span class="position mb-2">English Teacher</span>
+								<h3>Mr. Ndubuisi Ogwo <span>(B.Sc(Hons.); PGD; MIPMD; FIPMD)</span></h3>
+								<span class="position mb-2">Secretary</span>
 								<div class="faded">
-									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+									<p>Mr. Ndubuisi Ogwo is a banker of no mean repute. He is a financial consultant and has played key roles in the growth of many financial institutes in the country. </p>
+	              </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch">
+								<div class="img align-self-stretch" style="background-image: url(assets/img/testimonial-2.jpg);"></div>
+							</div>
+							<div class="text pt-3 text-center">
+								<h3>Chief Chris Uche <span>(SAN); (MIPMD; FIPMD)</span></h3>
+								<span class="position mb-2">Legal Adviser</span>
+								<div class="faded">
+									<p>Chief Uche SAN is a foremost Nigerian Legal practitioner with over 30 years of experience. As a result of his distinguished legal practic, he was elevated to the rank of a Senior Advocate of Nigeria in 2003.</p>
 	              </div>
 							</div>
 						</div>
@@ -48,10 +76,10 @@
 								<div class="img align-self-stretch" style="background-image: url(assets/img/team-1.jpg);"></div>
 							</div>
 							<div class="text pt-3 text-center">
-								<h3>Stella Smith</h3>
-								<span class="position mb-2">Art Teacher</span>
+								<h3>Dr. Sunday C. Nwite (BSS; MBA; M.Sc; Ph.D; ACII; IRDI, MIMPD; FIPMD)</h3>
+								<span class="position mb-2">Council Member</span>
 								<div class="faded">
-									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+									<p>Prof. Sunday Nwite is a professor in the Department of Banking and Finance Ebonyi State University - Abakaliki. His contribution iin Financial Administration has set him apart from many of his mates</p>
 	              </div>
 							</div>
 						</div>
@@ -59,69 +87,13 @@
 					<div class="col-md-6 col-lg-3 ftco-animate">
 						<div class="staff">
 							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(images/teacher-4.jpg);"></div>
+								<div class="img align-self-stretch" style="background-image: url(assets/img/team-1.jpg)"></div>
 							</div>
 							<div class="text pt-3 text-center">
-								<h3>Monshe Henderson</h3>
-								<span class="position mb-2">Science Teacher</span>
+								<h3>Engr. Prof. Sabit A. Olagote <span>Ph.D; FIPMD; JP</span> </h3>
+								<span class="position mb-2">Council Member</span>
 								<div class="faded">
-									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-	              </div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
-						<div class="staff">
-							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(images/teacher-5.jpg);"></div>
-							</div>
-							<div class="text pt-3 text-center">
-								<h3>Daniel Tribor</h3>
-								<span class="position mb-2">Teacher</span>
-								<div class="faded">
-									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-	              </div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
-						<div class="staff">
-							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(images/teacher-6.jpg);"></div>
-							</div>
-							<div class="text pt-3 text-center">
-								<h3>Mitch Parker</h3>
-								<span class="position mb-2">English Teacher</span>
-								<div class="faded">
-									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-	              </div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
-						<div class="staff">
-							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(images/teacher-7.jpg);"></div>
-							</div>
-							<div class="text pt-3 text-center">
-								<h3>Stella Smith</h3>
-								<span class="position mb-2">Art Teacher</span>
-								<div class="faded">
-									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-	              </div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
-						<div class="staff">
-							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(images/teacher-8.jpg);"></div>
-							</div>
-							<div class="text pt-3 text-center">
-								<h3>Monshe Henderson</h3>
-								<span class="position mb-2">Science Teacher</span>
-								<div class="faded">
-									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+									<p>Engr. Professor Sabit A. Olagote is a chief Lecturer/Research Professor, an Economist and Business Analyst. He is the former head of Department of Mechanical Engineering, Federal Polytechnic, IIaro.    </p>
 	              </div>
 							</div>
 						</div>
