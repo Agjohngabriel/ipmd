db11d9941b7fb71cae3d5736f493f071df2a9991
Agjohngabriel <agjohngabriel@gmail.com>
2020-02-24 23:56:02 +0100

    This is Nigeria, see how we living now

---
+++ benefitsOfMembership.php
@@ -0,0 +1,29 @@
+<?php $title = "Membership Benefits"; include 'inc/header.php'; ?>
+
+<section id="about_intro">
+    <div class="about_intro-content">
+        <h2>IPMD > Membership Benefits</h2>
+    </div>
+</section><!-- #intro -->
+
+<section id="services">
+      <div class="container">
+        <div class="section-header">
+          <h2>Membership Benefits</h2>
+          <p>As a member, the following are the benefits that are associated with membership.</p>
+          <ul>
+              <li class="py-2"><i class="ion-android-checkmark-circle"></i> Members shall have the right to use the appropriate titular letters: GMIPMD, AMIPMD, MIPMD, or FIPMD consistent with their membership category after their names, which confer professional honor, recognition, personality and status.</li>
+              <li class="py-2"><i class="ion-android-checkmark-circle"></i> Membership certificates approved by the Council and Management are issued to members under the institute's seal.</li>
+              <li class="py-2"><i class="ion-android-checkmark-circle"></i> Membership places you on the path of rapid career progression as it enhances your assessment and promotion to responsible position both in government and the organized private sector workplaces.</li>
+              <li class="py-2"><i class="ion-android-checkmark-circle"></i> Members are entitiled to to the institute's wall shield plaque, tie, car sticker, branded ball pen and an identification card.</li>
+              <li class="py-2"><i class="ion-android-checkmark-circle"></i> Updating of members' knowledge on current global practice in policy management and development as well as on local and international policy issues through news bulletins and the readership of the <i>THE POLICY ANALYST</i> journal.</li>
+              <li class="py-2"><i class="ion-android-checkmark-circle"></i> Mutually beneficial local and international socio-professional networking in diverse for personal and corperate advancements.</li>
+              <li class="py-2"><i class="ion-android-checkmark-circle"></i> Participation of members in local and international conference training, workshops and awards, etc at discounted rates.</li>
+              
+            </ul>
+        </div>
+      </div>
+</section>
+
+
+<?php include 'inc/footer.php'; ?>
--- brandedItems.php
+++ brandedItems.php
@@ -13,8 +13,8 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="portfolio-item wow fadeInUp">
-              <a href="img/portfolio/1.jpg" class="portfolio-popup">
-                <img src="img/portfolio/1.jpg" alt="">
+              <a href="assets/img/portfolio/1.jpg" class="portfolio-popup">
+                <img src="assets/img/portfolio/1.jpg" alt="">
                 <div class="portfolio-overlay">
                   <div class="portfolio-info"><h2 class="wow fadeInUp">Branded Item 1</h2></div>
                 </div>
@@ -24,8 +24,8 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="portfolio-item wow fadeInUp">
-              <a href="img/portfolio/2.jpg" class="portfolio-popup">
-                <img src="img/portfolio/2.jpg" alt="">
+              <a href="assets/img/portfolio/2.jpg" class="portfolio-popup">
+                <img src="assets/img/portfolio/2.jpg" alt="">
                 <div class="portfolio-overlay">
                   <div class="portfolio-info"><h2 class="wow fadeInUp">Branded Item 2</h2></div>
                 </div>
@@ -35,8 +35,8 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="portfolio-item wow fadeInUp">
-              <a href="img/portfolio/3.jpg" class="portfolio-popup">
-                <img src="img/portfolio/3.jpg" alt="">
+              <a href="assets/img/portfolio/3.jpg" class="portfolio-popup">
+                <img src="assets/img/portfolio/3.jpg" alt="">
                 <div class="portfolio-overlay">
                   <div class="portfolio-info"><h2 class="wow fadeInUp">Branded Item 3</h2></div>
                 </div>
@@ -46,8 +46,8 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="portfolio-item wow fadeInUp">
-              <a href="img/portfolio/4.jpg" class="portfolio-popup">
-                <img src="img/portfolio/4.jpg" alt="">
+              <a href="assets/img/portfolio/4.jpg" class="portfolio-popup">
+                <img src="assets/img/portfolio/4.jpg" alt="">
                 <div class="portfolio-overlay">
                   <div class="portfolio-info"><h2 class="wow fadeInUp">Branded Item 4</h2></div>
                 </div>
@@ -57,8 +57,8 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="portfolio-item wow fadeInUp">
-              <a href="img/portfolio/5.jpg" class="portfolio-popup">
-                <img src="img/portfolio/5.jpg" alt="">
+              <a href="assets/img/portfolio/5.jpg" class="portfolio-popup">
+                <img src="assets/img/portfolio/5.jpg" alt="">
                 <div class="portfolio-overlay">
                   <div class="portfolio-info"><h2 class="wow fadeInUp">Branded Item 5</h2></div>
                 </div>
@@ -68,8 +68,8 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="portfolio-item wow fadeInUp">
-              <a href="img/portfolio/6.jpg" class="portfolio-popup">
-                <img src="img/portfolio/6.jpg" alt="">
+              <a href="assets/img/portfolio/6.jpg" class="portfolio-popup">
+                <img src="assets/img/portfolio/6.jpg" alt="">
                 <div class="portfolio-overlay">
                   <div class="portfolio-info"><h2 class="wow fadeInUp">Branded Item 6</h2></div>
                 </div>
@@ -79,8 +79,8 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="portfolio-item wow fadeInUp">
-              <a href="img/portfolio/7.jpg" class="portfolio-popup">
-                <img src="img/portfolio/7.jpg" alt="">
+              <a href="assets/img/portfolio/7.jpg" class="portfolio-popup">
+                <img src="assets/img/portfolio/7.jpg" alt="">
                 <div class="portfolio-overlay">
                   <div class="portfolio-info"><h2 class="wow fadeInUp">Branded Item 7</h2></div>
                 </div>
@@ -90,8 +90,8 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="portfolio-item wow fadeInUp">
-              <a href="img/portfolio/8.jpg" class="portfolio-popup">
-                <img src="img/portfolio/8.jpg" alt="">
+              <a href="assets/img/portfolio/8.jpg" class="portfolio-popup">
+                <img src="assets/img/portfolio/8.jpg" alt="">
                 <div class="portfolio-overlay">
                   <div class="portfolio-info"><h2 class="wow fadeInUp">Branded Item 8</h2></div>
                 </div>
