03e764473f5db5da575b113cfa993bc4af668f3c
agjohngabriel <agjohngabriel@gmail.com>
2020-03-13 15:35:39 -0700

    Added all advisory board members

--- advisory_board.php
+++ advisory_board.php
@@ -15,87 +15,51 @@
 		<section class="ftco-section ftco-no-pb">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-6 col-lg-3 ftco-animate">
+				<div class="col-lg-6 ftco-animate">
 						<div class="staff">
 							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(assets/img/team-1.jpg);"></div>
+								<div class="img align-self-stretch" style="background-image: url(assets/img/testimonial-8.jpg);"></div>
 							</div>
-							<div class="text pt-3 text-center">
-								<h3>Sunday A. Eke <span>(Dip. M; MCM (UK); MBIM (Lond); MIPMD, FIPMD)</span></h3>
-								<span class="position mb-2">National President</span>
-								<div class="faded">
-									<p>Mr. Sunday Eke was formerly the chairman of the Mericlaims Agencies Nigeria Ltd. Before he proceedde to london for his professional education in Marketing and Management. </p>
-	              </div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
-						<div class="staff">
-							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(assets/img/testimonial-1.jpg);"></div>
-							</div>
-							<div class="text pt-3 text-center">
-								<h3>Professor Jacinta A. Opara <span>Bsc, Msc, MED, MBA, PHD, FIPMD, FIMC, FCIA, FBQS, FASI</span> </h3>
-								<span class="position mb-2">Chairman</span>
-								<div class="faded">
-									<p>Professor Jacinta A. Opara is a professor of international pedigress with proven track record</p>
-	              </div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
-						<div class="staff">
-							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(assets/img/team-1.jpg);"></div>
-							</div>
-							<div class="text pt-3 text-center">
-								<h3>Mr. Ndubuisi Ogwo <span>(B.Sc(Hons.); PGD; MIPMD; FIPMD)</span></h3>
-								<span class="position mb-2">Secretary</span>
-								<div class="faded">
-									<p>Mr. Ndubuisi Ogwo is a banker of no mean repute. He is a financial consultant and has played key roles in the growth of many financial institutes in the country. </p>
-	              </div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
-						<div class="staff">
-							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(assets/img/testimonial-2.jpg);"></div>
-							</div>
-							<div class="text pt-3 text-center">
-								<h3>Chief Chris Uche <span>(SAN); (MIPMD; FIPMD)</span></h3>
-								<span class="position mb-2">Legal Adviser</span>
-								<div class="faded">
-									<p>Chief Uche SAN is a foremost Nigerian Legal practitioner with over 30 years of experience. As a result of his distinguished legal practic, he was elevated to the rank of a Senior Advocate of Nigeria in 2003.</p>
-	              </div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
-						<div class="staff">
-							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(assets/img/team-1.jpg);"></div>
-							</div>
-							<div class="text pt-3 text-center">
+							<div class="text pt-2 text-center">
 								<h3>Dr. Sunday C. Nwite (BSS; MBA; M.Sc; Ph.D; ACII; IRDI, MIMPD; FIPMD)</h3>
-								<span class="position mb-2">Council Member</span>
+								<span class="position mb-2">CHAIRMAN</span>
 								<div class="faded">
 									<p>Prof. Sunday Nwite is a professor in the Department of Banking and Finance Ebonyi State University - Abakaliki. His contribution iin Financial Administration has set him apart from many of his mates</p>
+									<P>He is a well read scholar with experience. As a Teacher, Educationist and an Financial Administrator he has held many positions in different higher institutions of learning in Nigeria.</P>
 	              </div>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-6 col-lg-3 ftco-animate">
+					<div class="col-lg-6 ftco-animate">
 						<div class="staff">
 							<div class="img-wrap d-flex align-items-stretch">
-								<div class="img align-self-stretch" style="background-image: url(assets/img/team-1.jpg)"></div>
+								<div class="img align-self-stretch" style="background-image: url(assets/img/testimonial-3.jpg);"></div>
 							</div>
-							<div class="text pt-3 text-center">
-								<h3>Engr. Prof. Sabit A. Olagote <span>Ph.D; FIPMD; JP</span> </h3>
-								<span class="position mb-2">Council Member</span>
-								<div class="faded">
-									<p>Engr. Professor Sabit A. Olagote is a chief Lecturer/Research Professor, an Economist and Business Analyst. He is the former head of Department of Mechanical Engineering, Federal Polytechnic, IIaro.    </p>
-	              </div>
+							<div class="text pt-2 text-center">
+								<h3>Prof. Carol Chinyere Opara<span>B.A.Ed; M.Ed; Ph.D; FIPMD</span></h3>
+								<span class="position mb-2">Members</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch">
+								<div class="img align-self-stretch" style="background-image: url(assets/img/testimonial-7.jpg);"></div>
+							</div>
+							<div class="text pt-2 text-center">
+								<h3>Prof. (Mrs) Regina Uwaoma Obi, B.Sc, MSc, Ph.D, FIPMD, ABMIPMD, NIH,A, NDMAA, LAP, AICA, BEPCWOAES, MAERD</h3>
+								<span class="position mb-2">Member</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch">
+								<div class="img align-self-stretch" style="background-image: url(assets/img/member1.jpg);"></div>
+							</div>
+							<div class="text pt-2 text-center">
+								<h3>Chief Bar. Dr. (Mrs.) Caroline Mbafan Ekpendu, JP. LLB (Hons), BL, LLM, Ph.D, FIPMD.</h3>
+								<span class="position mb-2">Member</span>
 							</div>
 						</div>
 					</div>
